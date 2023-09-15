07ce31e8b9706a6557292c1366c1c14ec6f5f6c9
rubenangber <rubigrowl@gmail.com>
2023-09-15 15:53:41 +0200

    Hola mundo

--- Project1/Project1/CodeFile1.cs
+++ Project1/Project1/CodeFile1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+
 namespace Proyectos.ElSegundo {
+    
     class Basica {
         [STAThread]
         public static void Main() {
