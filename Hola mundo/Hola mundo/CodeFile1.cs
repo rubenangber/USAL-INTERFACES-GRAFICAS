ec5b3b352316fc470f96276fe7f691ff77dd49c3
rubenangber <rubigrowl@gmail.com>
2023-09-17 20:59:36 +0200

    Hola mundo

---
+++ Hola mundo/Hola mundo/CodeFile1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Proyectos.ElSegundo {
+
+    class Basica {
+        [STAThread]
+
+        public static void Main() {
+            Window win = new Window();
+            win.Title = "Primer Programa";
+            win.Show();
+            Label etiqueta = new Label();
+            etiqueta.Content = "Hola Mundo C#";
+            win.Content = etiqueta;
+            Application app = new Application();
+            Console.WriteLine("Empezando");
+            app.Run();
+            Console.WriteLine("Terminando");
+        }
+    }
+}
