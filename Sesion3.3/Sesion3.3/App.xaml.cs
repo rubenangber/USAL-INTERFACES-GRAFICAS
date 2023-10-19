f0a01d9213eac3a33fdb875f0a5f80f7d628f30b
rubenangber <rubigrowl@gmail.com>
2023-10-19 12:14:46 +0200

    Sesion 3.3

---
+++ Sesion3.3/Sesion3.3/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sesion3._3
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
