f0e2691bce7268fff7c624216921e31c8214818b
rubenangber <rubigrowl@gmail.com>
2023-10-20 20:05:07 +0200

    Sesion 3.4

---
+++ Sesion 3.4/Sesion 3.4/NumericTextBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Sesion_3._4
+{
+    internal class NumericTextBox:TextBox {
+        public NumericTextBox() {
+        
+        }
+    }
+}
