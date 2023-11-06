635588b0bad0f48b58a171823654f970287dcb9d
rubenangber <rubigrowl@gmail.com>
2023-11-06 22:48:41 +0100

    EL PACTOMETRO

--- EL PACTÓMETRO/EL PACTÓMETRO/MainWindow.xaml.cs
+++ EL PACTÓMETRO/EL PACTÓMETRO/MainWindow.xaml.cs
@@ -1,28 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace EL_PACTÓMETRO
-{
+
+namespace EL_PACTÓMETRO {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
+        }
+
+        private void MostrarMenu_Click(object sender, RoutedEventArgs e) {
+            if (sender is Button button) {
+                // Obtén el menú contextual del botón
+                ContextMenu contextMenu = button.ContextMenu;
+
+                if (contextMenu != null) {
+                    // Abre o cierra el menú contextual
+                    contextMenu.IsOpen = !contextMenu.IsOpen;
+                }
+            }
+        }
+        private void AbrirPagina2_Click(object sender, RoutedEventArgs e) {
+            //Crear segunda ventana
+            Ventana1 v1 = new Ventana1();
+            //Cerrar ventana actual
+            this.Close();
+            //Mostrar v1
+            v1.Show();
+            
         }
     }
 }
