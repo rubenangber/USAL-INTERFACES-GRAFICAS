adaf8ce3769aaf8af207242350aa32ea477491f4
rubenangber <rubigrowl@gmail.com>
2023-11-07 23:16:44 +0100

    EL PACTOMETRO

--- EL PACTÓMETRO/EL PACTÓMETRO/MainWindow.xaml.cs
+++ EL PACTÓMETRO/EL PACTÓMETRO/MainWindow.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Navigation;
-
+using System.Windows.Shapes;
 
 namespace EL_PACTÓMETRO {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow() {
@@ -32,7 +30,35 @@ namespace EL_PACTÓMETRO {
             this.Close();
             //Mostrar v1
             v1.Show();
-            
+        }
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e) {
+            partidos partidos = new partidos();
+            // Borra cualquier contenido existente en el Canvas cuando se maximiza y minimiza la pantalla
+            miCanvas.Children.Clear();
+
+            // Obtiene el ancho y alto actual del Canvas.
+            double anchoCanvas = miCanvas.ActualWidth;
+            double altoCanvas = miCanvas.ActualHeight;
+
+            //PP
+            Rectangle pp = new Rectangle();
+            pp.Width = anchoCanvas / 24;
+            pp.Height = (partidos.getVotosPP()) - (altoCanvas / 12);
+            pp.Fill = new SolidColorBrush(Colors.Blue);
+
+            Canvas.SetLeft(pp, anchoCanvas / 24 * 3);
+            Canvas.SetBottom(pp, partidos.getVotosPP() / 12);
+            miCanvas.Children.Add(pp);
+
+            //PSOE
+            Rectangle psoe = new Rectangle();
+            psoe.Width = anchoCanvas / 24;
+            psoe.Height = altoCanvas / 12;
+            psoe.Fill = new SolidColorBrush(Colors.Red);
+
+            Canvas.SetLeft(psoe, anchoCanvas / 24 * 5);
+            Canvas.SetBottom(psoe, 10);
+            miCanvas.Children.Add(psoe);
         }
     }
 }
---
+++ EL PACTÓMETRO/EL PACTÓMETRO/partidos.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL_PACTÓMETRO
+{
+    internal class partidos {
+        private int votosPP = 124;
+        private int votosPSOE = 30;
+        private int votosVOX;
+        private int votosSUMAR;
+        private int votosERC;
+        private int votosJUNTS;
+        private int votosBILDU;
+        private int votosPNV;
+        private int votosBNG;
+        private int votosCCA;
+        private int votosUPN;
+
+        public int getVotosPP()
+        {
+            return votosPP;
+        }
+        public void setVotosPP(int votosPP)
+        {
+            this.votosPP = votosPP;
+        }
+        public int getVotosPSOE()
+        {
+            return votosPSOE;
+        }
+        public void setVotosPSOE(int votosPSOE)
+        {
+            this.votosPSOE = votosPSOE;
+        }
+        public int getVotosVOX()
+        {
+            return votosVOX;
+        }
+        public void setVotosVOX(int votosVOX)
+        {
+            this.votosVOX = votosVOX;
+        }
+        public int getVotosSUMAR()
+        {
+            return votosSUMAR;
+        }
+        public void setVotosSUMAR(int votosSUMAR)
+        {
+            this.votosSUMAR = votosSUMAR;
+        }
+        public int getVotosERC()
+        {
+            return votosERC;
+        }
+        public void setVotosERC(int votosERC)
+        {
+            this.votosERC = votosERC;
+        }
+        public int getVotosJUNTS()
+        {
+            return votosJUNTS;
+        }
+        public void setVotosJUNTS(int votosJUNTS)
+        {
+            this.votosJUNTS = votosJUNTS;
+        }
+        public int getVotosBILDU()
+        {
+            return votosBILDU;
+        }
+        public void setVotosBILDU(int votosBILDU)
+        {
+            this.votosBILDU = votosBILDU;
+        }
+        public int getVotosPNV()
+        {
+            return votosPNV;
+        }
+        public void setVotosPNV(int votosPNV)
+        {
+            this.votosPNV = votosPNV;
+        }
+        public int getVotosBNG()
+        {
+            return votosBNG;
+        }
+        public void setVotosBNG(int votosBNG)
+        {
+            this.votosBNG = votosBNG;
+        }
+        public int getVotosCCA()
+        {
+            return votosCCA;
+        }
+        public void setVotosCCA(int votosCCA)
+        {
+            this.votosCCA = votosCCA;
+        }
+        public int getVotosUPN()
+        {
+            return votosUPN;
+        }
+        public void setVotosUPN(int votosUPN)
+        {
+            this.votosUPN = votosUPN;
+        }
+    }
+}
